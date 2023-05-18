289fee6f27f0940bad1bd2aebf932b0633daba0f
YusufHasan94 <yhpolok@gmail.com>
2023-05-19 01:55:34 +0600

    adding review data

--- index.js
+++ index.js
@@ -6,12 +6,20 @@ app.use(cors());
 app.use(express.json());
 
 const images = require('./data/images.json');
+const categories = require('./data/categories.json');
+const review = require('./data/review.json');
 
 app.get('/', (req, res)=>{
     res.send("Hello Disney Toy server");
 })
 app.get('/images',(req, res)=>{
     res.send(images);
+})
+app.get('/categories',(req, res)=>{
+    res.send(categories);
+})
+app.get('/review',(req, res)=>{
+    res.send(review);
 })
 app.listen(port, ()=>{
     console.log(`server run successfully to ${port}`)
