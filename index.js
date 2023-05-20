deae5e0c23781fafe8c0c2e8e636122189e988b2
YusufHasan94 <yhpolok@gmail.com>
2023-05-20 22:44:47 +0600

    adding delete route

--- index.js
+++ index.js
@@ -37,18 +37,21 @@ async function run() {
     const category = database.collection("category");
     const toysCollection = database.collection("toys");
 
+    //set route to get images for gallery
     app.get('/images',async(req, res)=>{
         const cursor = images.find();
         const result = await cursor.toArray();
         res.send(result);
     })
 
+    //set route to get data for category tabs
     app.get('/categories', async(req, res)=>{
         const cursor = category.find();
         const result = await cursor.toArray();
         res.send(result);
     })   
     
+    //set route to get specific subcategory
     app.get('/categories/:id', async(req, res)=>{
         const id = req.params.id;
         const query = {_id: new ObjectId(id)};
@@ -56,37 +59,43 @@ async function run() {
         res.send(result);
     })
 
+    //set route to get data for user reviews
     app.get('/reviews',async(req, res)=>{
         const cursor = reviews.find();
         const result = await cursor.toArray();
         res.send(result);
     })
     
+    //set route to get images for sponsors
     app.get('/sponsors',async(req, res)=>{
         const cursor = sponsors.find();
         const result = await cursor.toArray();
         res.send(result);
     })
 
+    //set route to add toy
     app.post('/toys', async(req, res)=>{
         const toys = req.body;
         const result = await toysCollection.insertOne(toys);
         res.send(result);
     })
 
+    //set route to get data for all toys 
     app.get('/toys', async(req, res)=>{
         const cursor = toysCollection.find();
         const result = await cursor.toArray();
         res.send(result);
     })
 
+    //set route to get data for specific toy
     app.get('/toys/:id', async(req, res)=>{
         const id = req.params.id;
         const query = {_id: new ObjectId(id)};
         const result = await toysCollection.findOne(query);
         res.send(result);
     })
-    
+
+    //view user added toys using filter
     app.get('/my-toys', async(req, res)=>{
         let query = {};
         if(req.query?.email){
@@ -94,6 +103,21 @@ async function run() {
         }
         const cursor = toysCollection.find(query);
         const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    //view user selected toy
+    app.get('/my-toys/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await toysCollection.findOne(query);
+        res.send(result);
+    })
+    //delete selected toys
+    app.delete('/my-toys/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await toysCollection.deleteOne(query);
         res.send(result);
     })
 
